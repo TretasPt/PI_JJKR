2fcda657bf6fcf0f697256c64ca4b384226af8cc
TretasPt <86566152+TretasPt@users.noreply.github.com>
2023-10-10 10:50:38 +0100

    Updated movement.
    Movement is now based on velocity changes instead of direct position changes.

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,11 +6,13 @@ public class PlayerMotor : MonoBehaviour
 {
 
     private CharacterController controller;
-    private Vector3 playerVelocity;
+    [SerializeField] private Vector3 playerVelocity;
     private bool isGrounded;
     public float speed = 5f;
-    public float gravity = -9.8f;
+    public float friction = 1.5f;
+    public float gravity = -20f;//-9.8f;
     public float jumpHeight = 3f;
+    public bool airMovement = false;
 
 
     // Start is called before the first frame update
@@ -27,23 +29,44 @@ public class PlayerMotor : MonoBehaviour
 
     public void ProcessMove(Vector2 input)
     {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
 
-        controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
 
-        playerVelocity.y += gravity * Time.deltaTime;
-        if(isGrounded && playerVelocity.y < 0)
+
+        if (isGrounded || airMovement)
+        {
+            playerVelocity.x += input.x * speed;
+            playerVelocity.z += input.y * speed;
+            // Vector3 moveDirection = Vector3.zero;
+            // moveDirection.x = input.x;
+            // moveDirection.z = input.y;
+
+            // controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+
+            playerVelocity.x /= friction;
+            playerVelocity.z /= friction;
+        }
+        if (!isGrounded)
+        {
+            playerVelocity.y += gravity * Time.deltaTime;
+        }
+
+
+
+
+
+        if (isGrounded && playerVelocity.y < 0)
         {
             playerVelocity.y = -2f;
         }
-        controller.Move(playerVelocity * Time.deltaTime);
+
+        controller.Move(transform.TransformDirection(playerVelocity) * Time.deltaTime);
+
+
     }
 
     public void Jump()
     {
-        if(isGrounded)
+        if (isGrounded)
         {
             playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
         }
