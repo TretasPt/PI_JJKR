3a00617fc192d044b12e4b09be553c369ab0bcdb
TretasPt <86566152+TretasPt@users.noreply.github.com>
2023-11-12 18:10:50 +0000

    Rudimentary tree spawning colision.
    Don't run this version. This is a backup as unity crashed, likely due to an infinite cycle.

--- Assets/Scripts/Trees.cs
+++ Assets/Scripts/Trees.cs
@@ -78,9 +78,6 @@ public class Trees : MonoBehaviour
     void Start()
     {
         Debug.Log("Start.");
-
-
-
     }
 
     // Update is called once per frame
@@ -103,7 +100,6 @@ public class Trees : MonoBehaviour
     private int GenerateNumberOfForestCluesters(int min, int max)
     {
         //TODO Make random
-        Debug.Log("Returning a number of tree clusters." + max);
         return (min + max) / 2;
     }
 
@@ -135,43 +131,44 @@ public class Trees : MonoBehaviour
         }
     }
 
-    private void GenerateForestCluster(int numberOfTrees, GameObject treeClusterParent)
+    private void GenerateForestCluster(int numberOfTrees, GameObject treeClusterParent, float treeRatio = 0.7f)
     //TODO
     {
 
         float x = Random.Range(-100f, 100f);
         float z = Random.Range(-100f, 100f);
 
-        // treeClusterParent.transform.position.Set(x,0,z);
         treeClusterParent.transform.SetLocalPositionAndRotation(new Vector3(x, 0, z), Quaternion.identity);
 
         Debug.Log(treeClusterParent.transform.position);
 
-        // GameObject centralTree = PlaceProp(treeClusterParent.transform.position, Quaternion.identity, treeClusterParent.transform);
-        GameObject centralTree = PlaceProp(origin, Quaternion.identity, treeClusterParent.transform);
-        // Debug.Log(centralTree.transform.position);
-        // centralTree.GetComponent("Sphere").AddComponent();;
-        // centralTree.GetComponent<MeshRenderer>().material = RedLeafMaterial;
-        // centralTree.GetComponent("Sphere").GetComponent<MeshRenderer>().material = RedLeafMaterial;
-        centralTree.transform.Find("Sphere").GetComponent<MeshRenderer>().material = RedLeafMaterial;
+        GenerateTree(origin, Quaternion.identity, treeClusterParent.transform, 2, 3, RedLeafMaterial);
 
-        for (int i = 1; i < numberOfTrees; i++)
+        int treesToPlace = numberOfTrees;
+
+        while (treesToPlace > 0)
         {
-            // PlaceRandomTree(treeClusterParent.transform);
-            float angle = Random.Range(0, 2 * Mathf.PI);
-            float radious = Random.Range(20, 40);
 
-            bool isTree = Random.Range(0f, 1f) > 0.5;
+            int lastTree = (int)(treesToPlace * treeRatio);
 
-            PlaceProp(PolarToCartesian(angle, radious), Quaternion.identity, treeClusterParent.transform, isTree);
+            for (int i = 1; i < treesToPlace; i++)
+            {
+                float angle = Random.Range(0, 2 * Mathf.PI);
+                float radious = Random.Range(20, 40);
 
+                if(PlaceProp(PolarToCartesian(angle, radious), Quaternion.identity, treeClusterParent.transform, i<lastTree)!=null){
+                    treesToPlace--;
+                }
+
+            }
         }
+
+
         Debug.Log("Generating a cluster with " + numberOfTrees + " trees.");
     }
 
     private GameObject PlaceProp(Vector3 position, Quaternion rotation, Transform parent, bool isTree = true)
     {
-        // return Instantiate(isTree ? TreePrefab : BushPrefab, position, rotation, parent);
         if (isTree)
         {
             return GenerateTree(position, rotation, parent);
@@ -182,30 +179,6 @@ public class Trees : MonoBehaviour
         }
     }
 
-    // private GameObject PlaceProp(Vector3 position, Quaternion rotation, bool isTree = true)
-    // {
-    //     return PlaceProp(position, rotation, transform, isTree);
-    // }
-
-    // private GameObject PlaceProp(bool isTree = true)
-    // {
-    //     return PlaceProp(transform.position, Quaternion.identity, isTree);
-    // }
-
-    // private GameObject PlaceRandomProp(Transform parent, bool isTree = true)
-    // {
-    //     // Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
-    //     Vector3 randomPosition = parent.position + Random.insideUnitSphere * radius;
-    //     randomPosition.y = 0;
-    //     return PlaceProp(randomPosition, Quaternion.identity, parent, isTree);
-    // }
-
-    // private GameObject PlaceRandomProp(bool isTree = true)
-    // {
-    //     Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
-    //     randomPosition.y = 0;
-    //     return PlaceProp(randomPosition, Quaternion.identity, isTree);
-    // }
 
     /// <summary>
     /// Translates a given 2d polar coordinate into a Vector3 cartesian coordinate with height 0.
@@ -230,10 +203,27 @@ public class Trees : MonoBehaviour
     private GameObject GenerateTree(Vector3 position, Quaternion rotation, Transform parentCluster, float height = 1, float width = 1, Material leafMaterialOverride = null)
     {
 
-        GameObject tree = Instantiate(TreePrefab, position + parentCluster.position, rotation, parentCluster);
+        position += parentCluster.position;
+
+        foreach (var collider in Physics.OverlapCapsule(position, position + new Vector3(0, 10, 0), 1))
+        {
+            if (collider.gameObject.tag == "Tree")
+            {
+                Debug.Log("Skipped one.");
+                return null;
+            }
+        }
+
+        GameObject tree = Instantiate(TreePrefab, position, rotation, parentCluster);
+
 
         Vector3 scale = new Vector3(width, height, width);
         tree.transform.localScale = Vector3.Scale(scale, tree.transform.localScale);
+
+        // if(Physics.CheckCapsule(position,position+new Vector3(0,10,0),5)){
+        //     Debug.Log("Bati.");
+        //     // return null;
+        // }
 
         if (leafMaterialOverride)
         {
@@ -258,6 +248,8 @@ public class Trees : MonoBehaviour
     {
         GameObject bush = Instantiate(BushPrefab, position + parentCluster.position, rotation, parentCluster);
 
+        // bush.tag = "Bush";
+
         Vector3 scale = new Vector3(width, height, width);
         bush.transform.localScale = Vector3.Scale(scale, bush.transform.localScale);
 
@@ -271,4 +263,8 @@ public class Trees : MonoBehaviour
 
     }
 
+
+
 }
+
+
