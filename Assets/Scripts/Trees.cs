3f6ede7d00ed59fbb11eedecf6db7d87126cd601
TretasPt <86566152+TretasPt@users.noreply.github.com>
2023-11-12 13:20:34 +0000

    Update Trees.cs

--- Assets/Scripts/Trees.cs
+++ Assets/Scripts/Trees.cs
@@ -57,6 +57,9 @@ public class Trees : MonoBehaviour
     public int MAX_TREE_CLUSTERS = 10;
 
 
+
+
+
     void Awake()
     {
         Debug.Log("Awake.");
@@ -85,6 +88,11 @@ public class Trees : MonoBehaviour
     {
 
     }
+
+
+
+
+
 
     /// <summary>
     /// //TODO Make random
@@ -139,7 +147,8 @@ public class Trees : MonoBehaviour
 
         Debug.Log(treeClusterParent.transform.position);
 
-        GameObject centralTree = PlaceProp(treeClusterParent.transform.position, Quaternion.identity, treeClusterParent.transform);
+        // GameObject centralTree = PlaceProp(treeClusterParent.transform.position, Quaternion.identity, treeClusterParent.transform);
+        GameObject centralTree = PlaceProp(origin, Quaternion.identity, treeClusterParent.transform);
         // Debug.Log(centralTree.transform.position);
         // centralTree.GetComponent("Sphere").AddComponent();;
         // centralTree.GetComponent<MeshRenderer>().material = RedLeafMaterial;
@@ -173,30 +182,30 @@ public class Trees : MonoBehaviour
         }
     }
 
-    private GameObject PlaceProp(Vector3 position, Quaternion rotation, bool isTree = true)
-    {
-        return PlaceProp(position, rotation, transform, isTree);
-    }
+    // private GameObject PlaceProp(Vector3 position, Quaternion rotation, bool isTree = true)
+    // {
+    //     return PlaceProp(position, rotation, transform, isTree);
+    // }
 
-    private GameObject PlaceProp(bool isTree = true)
-    {
-        return PlaceProp(transform.position, Quaternion.identity, isTree);
-    }
+    // private GameObject PlaceProp(bool isTree = true)
+    // {
+    //     return PlaceProp(transform.position, Quaternion.identity, isTree);
+    // }
 
-    private GameObject PlaceRandomProp(Transform parent, bool isTree = true)
-    {
-        // Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
-        Vector3 randomPosition = parent.position + Random.insideUnitSphere * radius;
-        randomPosition.y = 0;
-        return PlaceProp(randomPosition, Quaternion.identity, parent, isTree);
-    }
+    // private GameObject PlaceRandomProp(Transform parent, bool isTree = true)
+    // {
+    //     // Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
+    //     Vector3 randomPosition = parent.position + Random.insideUnitSphere * radius;
+    //     randomPosition.y = 0;
+    //     return PlaceProp(randomPosition, Quaternion.identity, parent, isTree);
+    // }
 
-    private GameObject PlaceRandomProp(bool isTree = true)
-    {
-        Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
-        randomPosition.y = 0;
-        return PlaceProp(randomPosition, Quaternion.identity, isTree);
-    }
+    // private GameObject PlaceRandomProp(bool isTree = true)
+    // {
+    //     Vector3 randomPosition = origin + Random.insideUnitSphere * radius;
+    //     randomPosition.y = 0;
+    //     return PlaceProp(randomPosition, Quaternion.identity, isTree);
+    // }
 
     /// <summary>
     /// Translates a given 2d polar coordinate into a Vector3 cartesian coordinate with height 0.
@@ -218,9 +227,21 @@ public class Trees : MonoBehaviour
     /// <param name="height">Tree height multiplier.</param>
     /// <param name="width">Tree width multiplier.</param>
     /// <returns>A Tree GameObject.</returns>
-    private GameObject GenerateTree(Vector3 position, Quaternion rotation, Transform parentCluster, float height = 1, float width = 1)
+    private GameObject GenerateTree(Vector3 position, Quaternion rotation, Transform parentCluster, float height = 1, float width = 1, Material leafMaterialOverride = null)
     {
-        return Instantiate(TreePrefab, position + parentCluster.position, rotation, parentCluster);
+
+        GameObject tree = Instantiate(TreePrefab, position + parentCluster.position, rotation, parentCluster);
+
+        Vector3 scale = new Vector3(width, height, width);
+        tree.transform.localScale = Vector3.Scale(scale, tree.transform.localScale);
+
+        if (leafMaterialOverride)
+        {
+            tree.transform.Find("Sphere").GetComponent<MeshRenderer>().material = leafMaterialOverride;
+
+        }
+
+        return tree;
 
     }
 
@@ -233,11 +254,20 @@ public class Trees : MonoBehaviour
     /// <param name="height">Bush height multiplier.</param>
     /// <param name="width">Bush width multiplier.</param>
     /// <returns>A Bush GameObject.</returns>
-    private GameObject GenerateBush(Vector3 position, Quaternion rotation, Transform parentCluster, float height = 1, float width = 1)
+    private GameObject GenerateBush(Vector3 position, Quaternion rotation, Transform parentCluster, float height = 1, float width = 1, Material leafMaterialOverride = null)
     {
-        GameObject a = Instantiate(BushPrefab, position + parentCluster.position, rotation, parentCluster);
+        GameObject bush = Instantiate(BushPrefab, position + parentCluster.position, rotation, parentCluster);
 
-        return a;
+        Vector3 scale = new Vector3(width, height, width);
+        bush.transform.localScale = Vector3.Scale(scale, bush.transform.localScale);
+
+        if (leafMaterialOverride)
+        {
+            bush.transform.Find("Sphere").GetComponent<MeshRenderer>().material = leafMaterialOverride;
+
+        }
+
+        return bush;
 
     }
 
